e1de9167d792453167b2d59445df3cf8e82c1111
immacool <65336226+immacool@users.noreply.github.com>
2020-05-15 10:38:02 +0300

    Add files via upload

--- mail_finder.py
+++ mail_finder.py
@@ -1,9 +1,11 @@
-from colorama import Fore, init
-import click
 import re
-import requests
+
 import bs4
+import click
+import requests
+from colorama import Fore
 from requests.exceptions import MissingSchema
+from json import dump
 
 BANNER = f'''
 {Fore.RED}██████╗  ██████╗ ███████╗████████╗{Fore.BLUE}███╗   ███╗ █████╗ ███╗   ██╗
@@ -15,7 +17,7 @@ BANNER = f'''
 
            {Fore.RED}!только для образовательных целей!
       {Fore.BLUE}скрипт ищущий почты по регулярному выражению
-          {Fore.GREEN}mail_finder.py {Fore.WHITE}<ссылка(с "http(s)")>
+                {Fore.GREEN}mail_finder.py {Fore.WHITE}<ссылка>
            {Fore.RED}!только для образовательных целей!{Fore.RESET}
 '''
 
@@ -31,7 +33,8 @@ def request(url, *args, **kwargs):
 
 def find_mails(url):
     text = request(url).text
-    return re.findall(r'\w+@\w+\.\w+', text)
+    matches = re.findall(r'\w+@\w+\.\w+', text)
+    return list(set(matches))
 
 
 @click.command()
@@ -39,7 +42,8 @@ def find_mails(url):
 @click.option('-D', '--debug', 'debug', is_flag=True)
 @click.option('-d', '--depth', 'depth')
 @click.option('-f', '--file', 'file')
-def main(url, debug, file, depth=1):
+@click.option('-j', '--json', 'json')
+def main(url, debug, file, json, depth=1):
     mails = {}
 
     text = request(url).text
@@ -58,9 +62,28 @@ def main(url, debug, file, depth=1):
         except Exception:
             pass
 
+    tmp = []
+    blank_hrefs = []
+    for href, mails in mails.items():
+        if not mails:
+            blank_hrefs.append(href)
+        else:
+            tmp.append(href + '\n')
+            for mail in mails:
+                tmp.append(f'\t{mail}\n')
+            tmp.append('\n')
+    tmp.append('\nПо ссылкам идущим далее не найдено ни одной почты.\n')
+    [tmp.append(f'{bh}\n') for bh in blank_hrefs]
+
     if file:
-        with open(file, 'w'):
-            
+        with open(file, 'w', encoding='utf-8') as f_cursor:
+            f_cursor.writelines(tmp)
+    elif json:
+        # не работает!
+        with open(json, 'w', encoding='utf-8') as f_io:
+            dump(mails, f_io)
+    else:
+        print(*tmp)
 
 
 if __name__ == '__main__':
